d53f51e7f1bfc4bcf669336474adba874c1e93c2
robertodonnell00 <robertodonnell76@yahoo.ie>
2026-02-04 15:38:13 +0000

    Added calories entry screen to the app

--- app/src/main/java/ie/setu/calorietracker/MainActivity.kt
+++ app/src/main/java/ie/setu/calorietracker/MainActivity.kt
@@ -6,24 +6,30 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import ie.setu.calorietracker.data.FoodModel
+import ie.setu.calorietracker.ui.screens.CaloriesScreen
 import ie.setu.calorietracker.ui.theme.CalorieTrackerTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+
         setContent {
             CalorieTrackerTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    CalorieTrackerApp(modifier = Modifier)
                 }
             }
         }
@@ -31,17 +37,20 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun CalorieTrackerApp(modifier: Modifier = Modifier) {
+    val foods = remember { mutableStateListOf<FoodModel>() }
+
+    CaloriesScreen(
+        modifier = modifier,
+        foods = foods
     )
 }
 
+
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun CalorieTrackerAppPreview() {
     CalorieTrackerTheme {
-        Greeting("Android")
+        CalorieTrackerApp(modifier = Modifier)
     }
 }
